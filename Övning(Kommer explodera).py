62865d7a2cc658ac6478ec875427bb6f39970c78
John Öberg <john.oberg@elev.ga.ntig.se>
2026-09-04 11:11:48 +0200

    commit

---
+++ Övning(Kommer explodera).py
@@ -0,0 +1,13 @@
+Tal1 = int(input("Ange en heltal snälla. "))
+
+for x in range(Tal1):
+    print(x)
+    
+
+
+nummer1 = int(input("En heltal tack! "))
+nummer2 = 0
+while nummer2 < nummer1:
+    print(nummer2)
+    nummer2 +=1 
+print(nummer1)
